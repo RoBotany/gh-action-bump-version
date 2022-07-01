d2f5699276dd8f9e9d5bc3321630acd54fc1f248
Dima S <dmytro.suprun@robotany.ag>
2022-07-01 03:37:22 -0400

    Revert to old URL format.

--- index.js
+++ index.js
@@ -206,7 +206,7 @@ const workspace = process.env.GITHUB_WORKSPACE;
       );
     }
 
-    const remoteRepo = `https://${process.env.AUTH_TOKEN}@github.com/${process.env.GITHUB_REPOSITORY}.git`;
+    const remoteRepo = `https://${process.env.ACTOR}:${process.env.AUTH_TOKEN}@github.com/${process.env.GITHUB_REPOSITORY}.git`;
     if (process.env['INPUT_SKIP-TAG'] !== 'true') {
       await runInWorkspaceAndPrint('git', ['tag', newVersion]);
       if (process.env['INPUT_SKIP-PUSH'] !== 'true') {
